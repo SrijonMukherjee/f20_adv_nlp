af5f6605f2d8ceff8fc655bf625424f445301d28
KoalaChelsea <chelseawang1995@gmail.com>
2020-09-13 14:21:57 -0400

    Added parsing for org and locations

--- newswatch/index_docs_with_entities.py
+++ newswatch/index_docs_with_entities.py
@@ -1,5 +1,5 @@
 # Can we build out a script to index documents along with the entities?
-
+"""EDIT BY MATT WILCHEK FOR HOMEWORK 1 SUBMISSION"""
 # basic plan:
 # List documents in directory
 # loop through documents
@@ -19,7 +19,8 @@ import pysolr
 
 solr = pysolr.Solr('http://localhost:8983/solr/newswatch')
 solr.delete(q='*:*')
-data_dir = "/Users/teacher/repos/f20_adv_nlp/sample_data/news_data"
+os.chdir(r'C:\Users\chels\OneDrive\Documents\Matt_NLP\f20_adv_nlp\sample_data\news_data')
+data_dir = os.getcwd()
 
 index_files = [file for file in listdir(data_dir)]
 index_contents = []
@@ -30,21 +31,38 @@ for index_file in index_files:
     content = text.read()
 
     doc = nlp(content)
-    entities = []
+
+    # Add organization and location entity lists
+    person_entities = []
+    org_entities = []
+    loc_entities = []
+
+    # Add conditions if entity equals organization or location, then add if unique
     for ent in doc.ents:
         if ent.label_ == "PERSON":
-            if ent.text not in entities:
-                entities.append(ent.text)
+            if ent.text not in person_entities:
+                person_entities.append(ent.text)
+        if ent.label_ == "ORG":
+            if ent.text not in org_entities:
+                org_entities.append(ent.text)
+        if ent.label_ == "LOC":
+            if ent.text not in loc_entities:
+                loc_entities.append(ent.text)
 
-    temp_document = {}
-    temp_document["content_txt"] = content
-    temp_document["entities_ss"] = entities #" - ".join(entities)
+    # Add lists to temporary document dictionary
+    temp_document = {"content_txt": content, "people_ss": person_entities, "organizations_ss": org_entities,
+                     "locations_ss": loc_entities}
+
+    # Close documnt
     text.close()
-    #print(temp_document["entities_txt"])
+    # print(temp_document["entities_txt"])
+    # Add document dictionary to list of parsed documents
     index_contents.append(temp_document)
     counter += 1
     # if counter == 5:
     #     break
 
+# Solr add all parsed documents
 solr.add(index_contents)
+# Solr save commit of parsed documents
 solr.commit()
